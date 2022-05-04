95b9dfdb3b79516f858213318a645471640f9679
Thiago Nascimento <nascimenthiago@gmail.com>
2022-05-04 09:52:50 -0300

    Adding SideBar and TaskForm components

---
+++ src/components/SideBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <img src="../assets/logo.png" alt="" />
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent ({
+    name: 'SideBar'
+});
+</script>
+
+<style scoped>
+header {
+  padding: 1rem;
+  background: #0d3b66;
+  width: 100%;
+  height: 100vh;
+}
+@media only screen and (max-width: 768px) {
+    header {
+        padding: 2.5rem;
+        height: auto;
+    }
+}
+</style>
---
+++ src/components/TaskForm.vue
@@ -0,0 +1,39 @@
+<template>
+
+    <div class="box">
+        <div class="columns">
+            <div class="column is-8" role="form" aria-label="Task creation form">
+                <input type="text" class="input" placeholder="Task to Start">
+            </div>
+            <div class="column">
+                <div class="is-flex is-align-items is-justify-content-space-between">
+                    <section>
+                        <strong>00:00:00</strong>
+                    </section>
+                    <button class="button">
+                        <span class="icon">
+                            <i class="fas fa-play"></i>
+                        </span>
+                        <span>play</span>
+                    </button>
+                    <button class="button">
+                        <span class="icon">
+                            <i class="fas fa-stop"></i>
+                        </span>
+                        <span>stop</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'TaskForm'
+});
+</script>
+
