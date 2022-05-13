738a8c7c0fe8b545686341b5e177da1a91653415
Thiago Nascimento <nascimenthiago@gmail.com>
2022-05-13 18:22:51 -0300

    Refactoring to nested projects routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import TasksView from '../views/TasksView.vue'
 import ProjectsView from '../views/ProjectsView.vue'
 import ProjectForm from '@/views/projects/ProjectForm.vue'
+import ProjectList from '@/views/projects/ProjectList.vue'
 
 const routes: RouteRecordRaw[] = [
     {
@@ -11,20 +12,29 @@ const routes: RouteRecordRaw[] = [
     },
     {
         path: '/projects',
-        name: 'projects',
-        component: ProjectsView
-    },
-    {
-        path: '/projects/new',
-        name: 'new project',
-        component: ProjectForm
-    },
-    {
-        path: '/projects/:id',
-        name: 'edit project',
-        component: ProjectForm,
-        props: true
-    },
+        component: ProjectsView,
+        children: [
+            {
+                path: '',
+                name: 'projects',
+                component: ProjectList
+            },
+            {
+                path: 'new',
+                name: 'new project',
+                component: ProjectForm
+            },
+            {
+                path: ':id',
+                name: 'edit project',
+                component: ProjectForm,
+                props: true
+            },
+        ]
+
+        
+    }
+
 ]
 
 const router = createRouter({
--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,50 +1,14 @@
 <template>
-    <section class="projects">
+    <div class="projects">
         <h1 class="title">Projects</h1>
-        <router-link to="/projects/new" class="button">
-            <span class="icon is-small">
-                <i class="fas fa-plus"></i>
-            </span>
-        </router-link>
-        <table class="table is-fullwidth">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="proj in projects" :key="proj.id">
-                    <td>{{ proj.id }}</td>
-                    <td>{{ proj.name }}</td>
-                    <td>
-                        <router-link :to="`/projects/${proj.id}`" class="button">
-                            <span class="icon is-small">
-                                <i class="fas fa-pencil-alt"></i>
-                            </span>
-                        </router-link>
-                    </td>
-                </tr>
-            </tbody>
-        </table>    
-    </section>
+        <router-view></router-view>
+    </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, computed } from 'vue';
-import { useStore } from '@/store';
-
+import { defineComponent } from 'vue'
 export default defineComponent({
-
-    name: 'ProjectsView',
-
-    setup() {
-        const store = useStore()
-        return {
-            projects: computed(() => store.state.projects)
-        }
-    }
+    name: 'ProjectsView'
 })
 </script>
 
@@ -53,4 +17,3 @@ export default defineComponent({
     padding: 1.25rem;
 }
 </style>
-
